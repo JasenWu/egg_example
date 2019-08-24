dd2d94fbdeb3c0020861bd7084c36a45279cbeb9
wuxingsheng <wuxingsheng@qutoutiao.net>
2019-08-24 15:49:52 +0800

    add list delete done

--- app/controller/posts.js
+++ app/controller/posts.js
@@ -1,16 +1,58 @@
  
 exports.index = async (ctx) => {
-    ctx.body = '123';
+   const data =  await ctx.service.posts.index();
+
+   ctx.body ={
+       retCode:0,
+       retData:data,
+       retMsg:'成功'
+   }
+   
 };
 
-exports.new = async () => {};
+// exports.new = async (ctx) => {
+//     const data =  await ctx.service.posts.add({
+//         title:'title',
+//         details:'details',
+//     });
+//     ctx.body = data;
+ 
+// };
 
-exports.create = async () => {};
+exports.create = async (ctx) => {
+    const data =  await ctx.service.posts.create({
+        ...ctx.request.body
+    });
+    ctx.body = data;
 
-exports.show = async () => {};
+};
+
+exports.show = async (ctx) => {
+    const data =  await ctx.service.posts.show({
+        id:ctx.params.id
+    });
+    ctx.body = data;
+};
 
 exports.edit = async () => {};
 
-exports.update = async () => {};
+exports.update = async (ctx) => {
+    let params = {
+        ...ctx.params,
+        ...ctx.request.body
+    }
+    
+    const data =  await ctx.service.posts.update(params);
+    ctx.body = data;
+};
 
-exports.destroy = async () => {};
+exports.destroy = async (ctx) => {
+    let params = {
+        id:ctx.params.id
+    }
+    const data =  await ctx.service.posts.delete(params);
+    ctx.body = data;
+
+    
+     
+};
---
+++ app/service/posts.js
@@ -0,0 +1,63 @@
+const Service = require('egg').Service;
+
+class PostsService extends Service {
+    async index() {
+        const posts =await this.app.mysql.select('posts');
+        return posts;
+    }
+
+    async show(data) {
+        const d =await this.app.mysql.get('posts',data);
+        
+        return {
+            retCode:0,
+            retMsg:'成功',
+            retData:d
+        };
+
+        
+    }
+
+    async create(data) {
+        const d =await this.app.mysql.insert('posts',data);
+        if(d.affectedRows == 1)
+        return {
+            retCode:0,
+            retMsg:'成功',
+            retData:d
+        };
+
+        
+    }
+
+    async delete(data) {
+        const d =await this.app.mysql.delete('posts',data);
+
+        if(d.affectedRows == 1)
+        return {
+            retCode:0,
+            retMsg:'成功',
+            retData:d
+        };
+  
+    }
+
+    async update(data) {
+        const d =await this.app.mysql.update('posts',data);
+
+        if(d.affectedRows == 1)
+        return {
+            retCode:0,
+            retMsg:'成功',
+            retData:d
+        };
+
+        
+    }
+
+  
+
+
+ 
+}
+module.exports = PostsService;
